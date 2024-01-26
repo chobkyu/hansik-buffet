7616fdf20db475c0bb7f746ccfe7fed50a4f31e0
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-01-26 16:34:14 +0900

    update

--- lib/src/screen/login.dart
+++ lib/src/screen/login.dart
@@ -50,24 +50,25 @@ class _LoginScreenState extends State<LoginScreen> {
 
     if (token != null) {
       if (!mounted) return;
-      Navigator.push(
-        context,
-        PageRouteBuilder(
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            var begin = const Offset(0.0, 1.0);
-            var end = Offset.zero;
-            var curve = Curves.ease;
-            var tween =
-                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-            return SlideTransition(
-              position: animation.drive(tween),
-              child: child,
-            );
-          },
-          pageBuilder: (context, animation, secondaryAnimation) =>
-              const HomeScreen(),
-        ),
-      );
+      // Navigator.push(
+      //   context,
+      //   PageRouteBuilder(
+      //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      //       var begin = const Offset(0.0, 1.0);
+      //       var end = Offset.zero;
+      //       var curve = Curves.ease;
+      //       var tween =
+      //           Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      //       return SlideTransition(
+      //         position: animation.drive(tween),
+      //         child: child,
+      //       );
+      //     },
+      //     pageBuilder: (context, animation, secondaryAnimation) =>
+      //         const HomeScreen(),
+      //   ),
+      // );
+      Navigator.of(context).pop();
     }
   }
 
