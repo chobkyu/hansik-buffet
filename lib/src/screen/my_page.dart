cb7ca9d9c5652a1419070624e0cf2ff05aea10e0
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-01-30 14:50:56 +0900

    signup

--- lib/src/screen/my_page.dart
+++ lib/src/screen/my_page.dart
@@ -39,33 +39,34 @@ class _MyPageState extends State<MyPage> {
 
   void getUser() async {
     String? token = await storage.read(key: 'token');
-
+    print('이혜선 후장');
+    print(token);
     try {
       userData = await getUserData.getUserData(token!);
       print(userData);
       setState(() {});
     } catch (err) {
       print(err.toString());
-      await storage.delete(key: 'token');
-      if (!mounted) return;
-      Navigator.push(
-        context,
-        PageRouteBuilder(
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            var begin = const Offset(0.0, 1.0);
-            var end = Offset.zero;
-            var curve = Curves.ease;
-            var tween =
-                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-            return SlideTransition(
-              position: animation.drive(tween),
-              child: child,
-            );
-          },
-          pageBuilder: (context, animation, secondaryAnimation) =>
-              const LoginScreen(),
-        ),
-      );
+      // await storage.delete(key: 'token');
+      // if (!mounted) return;
+      // Navigator.push(
+      //   context,
+      //   PageRouteBuilder(
+      //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      //       var begin = const Offset(0.0, 1.0);
+      //       var end = Offset.zero;
+      //       var curve = Curves.ease;
+      //       var tween =
+      //           Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      //       return SlideTransition(
+      //         position: animation.drive(tween),
+      //         child: child,
+      //       );
+      //     },
+      //     pageBuilder: (context, animation, secondaryAnimation) =>
+      //         const LoginScreen(),
+      //   ),
+      // );
     }
   }
 
