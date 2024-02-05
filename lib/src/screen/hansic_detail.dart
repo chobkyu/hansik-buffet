7b22f971afb9627bed6bf73a5cefd42ec729b4cc
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-02-05 16:41:49 +0900

    add todo

--- lib/src/screen/hansic_detail.dart
+++ lib/src/screen/hansic_detail.dart
@@ -20,7 +20,144 @@ class _HansicDetailState extends State<HansicDetail> {
         preferredSize: Size.fromHeight(50),
         child: CustomAppBar(title: '한식 뷔페'),
       ),
-      body: Text(widget.latLng.toString()),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                color: Colors.amber,
+                height: 380,
+                width: 320,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                '수라 한식 뷔폐 응암점',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    '후기 (25)',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Text(
+                    '구글 별점  0',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+              const Divider(
+                thickness: 1.2,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          size: 30,
+                          color: Colors.amber[400],
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          '서울시 은평구 응암동 45-10 101호',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          size: 30,
+                          color: Colors.amber[400],
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          '사용자 별점 3.7 (참여자 100)',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.reviews,
+                          size: 30,
+                          color: Colors.amber[400],
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          '리뷰 보기 (참여자 25)',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.food_bank_rounded,
+                          size: 30,
+                          color: Colors.amber[400],
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          '메뉴',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[400],
+                ),
+                child: const Text(
+                  '리뷰 작성하기',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
