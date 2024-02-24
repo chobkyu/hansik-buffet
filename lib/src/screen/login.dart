97d7da833d43fdf528602b941e88d276a78f5050
cxzaqq <roy1228@naver.com>
2024-02-24 12:39:45 +0900

    로그인 시 홈 화면으로

--- lib/src/screen/login.dart
+++ lib/src/screen/login.dart
@@ -545,7 +545,12 @@ class _LoginScreenState extends State<LoginScreen> {
                         await storage.write(key: 'token', value: token);
 
                         if (!mounted) return;
-                        Navigator.of(context).pop(); //로그인 안되어있을 시 처리 예정
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: ((context) => const HomeScreen()),
+                          ),
+                        ); //로그인 안되어있을 시 처리 예정
                       } catch (err) {
                         print(err);
                         ScaffoldMessenger.of(context).showSnackBar(
