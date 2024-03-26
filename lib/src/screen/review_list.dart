69637b48899c635c6f24cac11ad243a9cd62170b
cxzaqq <roy1228@naver.com>
2024-03-27 00:26:07 +0900

    리뷰 텍스트 많을 때 UI 깨지는 현상 수정

--- lib/src/screen/review_list.dart
+++ lib/src/screen/review_list.dart
@@ -122,25 +122,28 @@ class _ReviewListState extends State<ReviewList> {
                                 ),
                               );
                             },
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.hansicName,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 23,
+                            child: SizedBox(
+                              width: 260,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    widget.hansicName,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 23,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  reviewList![index].review,
-                                  //'카리나는 이쁘다 ${entries[index]}',
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
+                                  Text(
+                                    reviewList![index].review,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                           RatingBar.builder(
